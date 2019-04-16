6faa727f465f799c40ea3725a4e18bda511d04ab
Vltnt23 <49682840+Vltnt23@users.noreply.github.com>
2019-04-16 13:39:36 +0100

    Initial commit

---
+++ Assets/Scripts/DashingMove.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashingMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float dashspeed;
+    private float dashtime;
+    public float startdashtime;
+    private int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        dashtime = startdashtime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.D) && Input.GetKeyDown(KeyCode.J)
+            )
+            {
+                direction = 2;
+
+            }
+            else if (Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.J)
+          )
+            {
+                direction = 1;
+
+            }
+            else if (Input.GetKeyDown(KeyCode.W) && Input.GetKeyDown(KeyCode.J)
+          )
+            {
+                direction = 3;
+            }
+            else if (Input.GetKeyDown(KeyCode.S) && Input.GetKeyDown(KeyCode.J)
+          )
+            {
+                direction = 4;
+            }
+        }
+        else
+        {
+            if (dashtime <= 0)
+            {
+                direction = 0;
+                dashtime = startdashtime;
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                dashtime -= Time.deltaTime;
+
+                if (direction == 1)
+                {
+                    rb.velocity = Vector2.left * dashspeed;
+                }
+                else if (direction == 2)
+                {
+                    rb.velocity = Vector2.right * dashspeed;
+                }
+                else if (direction == 3)
+                {
+                    rb.velocity = Vector2.up * dashspeed;
+                }
+                else if (direction == 4)
+                {
+                    rb.velocity = Vector2.down * dashspeed;
+                }
+
+            }
+        }
+    }
+}
+
+
